e7a1b5cc719b816ad8cd9aab47c43471d5f24efb
Aaishu Agarwal <91615907+Aaishuagarwal123@users.noreply.github.com>
2023-08-03 02:21:21 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -35,34 +35,55 @@ class Solution
     int spanningTree(int n, vector<vector<int>> adj[])
     {
         // code here
-        vector<vector<int>> edges;
-        for(int i=0;i<n;i++)
+    //     vector<vector<int>> edges;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(auto v:adj[i])
+    //         {
+    //             int dest=v[0];
+    //             int wt=v[1];
+    //           edges.push_back({wt,i,dest});
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         par[i]=i;
+    //         rank[i]=0;
+    //     }
+    //   sort(edges.begin(),edges.end());
+    //   int ans=0;
+    //   for(int i=0;i<edges.size();i++)
+    //   {
+    //       int u=edges[i][1];
+    //       int v=edges[i][2];
+    //       int wt=edges[i][0];
+    //       if(findpar(u)!=findpar(v))
+    //       {
+    //           unionn(u,v);ans+=wt;
+    //       }
+    //   }
+    //   return ans;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    vector<int> vis(n,0);
+    pq.push({0,0});
+    int ans=0;
+    while(!pq.empty())
+    {
+        int node=pq.top().second;
+        int wt=pq.top().first;
+        pq.pop();
+        if(vis[node])
+        continue;
+        vis[node]=1;
+        ans+=wt;
+        for(auto u:adj[node])
         {
-            for(auto v:adj[i])
-            {
-                int dest=v[0];
-                int wt=v[1];
-               edges.push_back({wt,i,dest});
-            }
+            //if(!vis[u[0]])
+            pq.push({u[1],u[0]});
         }
-        for(int i=0;i<n;i++)
-        {
-            par[i]=i;
-            rank[i]=0;
-        }
-      sort(edges.begin(),edges.end());
-      int ans=0;
-      for(int i=0;i<edges.size();i++)
-      {
-          int u=edges[i][1];
-          int v=edges[i][2];
-          int wt=edges[i][0];
-          if(findpar(u)!=findpar(v))
-          {
-              unionn(u,v);ans+=wt;
-          }
-      }
-      return ans;
+        
+    }
+        return ans;
     }
 };
 
